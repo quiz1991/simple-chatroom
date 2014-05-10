964545af39e0a95f034ac99df9fe5355ef4bb2e4
ZheQian <quizip@gmail.com>
2014-05-10 11:13:24 -0400

    updated

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Run<tt>npm install</tt>
 	1. express: Web application framework for node
 	2. jade: Node template engine for embedded javascript
 	3. socket.io: Socket for communication between clients and servers
+
+May 10th, 2014:
+	1. include jquery in jade file
+	2. scorllTop
+	3. listen keyup of "enter" for input
--- public/chat.js
+++ public/chat.js
@@ -1,32 +1,44 @@
-window.onload = function() {
+$(document).ready(function(){
   var messages = [];
   var socket = io.connect('http://localhost:8080');
-  var field = document.getElementById("field");
-  var sendButton = document.getElementById("send");
-  var content = document.getElementById("content");
-  var name = document.getElementById("name");
 
-  socket.on('message', function (data) {
-    if(data.message) {
+  var field = $("#field");
+  var sendButton = $("#send");
+  var content = $("#content");
+  var name = $("#name");
+
+  socket.on("message", function(data){
+    if(data.message){
       messages.push(data);
       var html = '';
-      for(var i=0; i<messages.length; i++) {
-          html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
-          html += messages[i].message + '<br />';
+      for(var i = 0; i < messages.length; i++){
+        html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+        html += messages[i].message + '<br />';
       }
-      content.innerHTML = html;
-    }else {
+      content.html(html);
+      content.scrollTop(content[0].scrollHeight);      
+    }else{
       console.log("There is a problem:", data);
     }
   });
- 
-  sendButton.onclick = function() {
-    if(name.value == "") {
-      alert("Please type your name!");
-    } else {
-      var text = field.value;
-      socket.emit('send', { message: text, username: name.value });
-      field.value = "";
+
+  field.keyup(function(e) {
+    if(e.keyCode == 13) {
+      sendMessage();
     }
-  };
-}
+  });
+
+  sendButton.click(function(){
+    sendMessage();
+  });
+
+  sendMessage = function(){
+    if(name.val() == ""){
+      alert("Please type your name!");
+    }else{
+      var text = field.val();
+      socket.emit('send', { message: text, username: name.val() });
+      field.html("");
+    }
+  }
+});
