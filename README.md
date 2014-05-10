3056586e743bf94927c2e854a48c9cd92b731849
ZheQian <quizip@gmail.com>
2014-05-09 22:01:11 -0400

    functions completed

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 == Install node modules specified in package.json:
 Run<tt>npm install</tt>
+
+== Modules included
+	1. express: Web application framework for node
+	2. jade: Node template engine for embedded javascript
+	3. socket.io: Socket for communication between clients and servers
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var express = require('express');
-var app = express(); 
+var app = express();
+var port = 8080; 
 
+app.set('views', __dirname + '/templates');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
 app.get("/", function(req, res){
-	res.send("It works");
+  res.render("page");
 });
 
-app.listen(8080);
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function(socket){
+  socket.emit('message', { message: 'Welcome to the chatroom!' });
+  socket.on('send', function(data){
+    io.sockets.emit('message', data);
+  });
+});
+
 console.log("listening on port 8080");
