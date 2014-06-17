dfb33ad8907c46735d3e543d4809c8ec92c80613
ZheQian <quizip@gmail.com>
2014-06-16 22:55:35 -0400

    bubble message css

--- public/chat.js
+++ public/chat.js
@@ -29,9 +29,9 @@ $(document).ready(function(){
 
   socket.on('messages', function(message) {
     if(message.name === nickname)
-      content.append("<li class='floatRight message'>" + message.data + "</li>");
+      content.append("<li class='floatRight'><div class='message'>" + message.data + "</div>" + "<div class='right-triangle'></div>" + "</li>");
     else
-      content.append("<li class='floatLeft'>" + message.name + ": " + "<div class='left-triangle'></div><div class='message'>" + message.data + "</div>" + "</li>");
+      content.append("<li><div class='left-triangle'></div><div class='message'>" + message.data + "</div>" + "</li>");
 
     // set the scroll bar to the newest message
     content.scrollTop(content[0].scrollHeight);
@@ -53,7 +53,7 @@ $(document).ready(function(){
 
     // clean the message input area after sent
     field.val("");
-    content.append("<li class='floatRight message'>" + message + "</li>");
+    content.append("<li class='floatRight'><div class='message'>" + message + "</div>" + "<div class='right-triangle'></div>" + "</li>");
     content.scrollTop(content[0].scrollHeight);
     socket.emit('messages', message);
   }
